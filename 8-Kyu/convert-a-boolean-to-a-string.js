e85fe039a48467aca7931e14f76bfeab004671b0
Edugiyuu <102703153+Edugiyuu@users.noreply.github.com>
2022-09-11 17:45:56 -0300

    Convert a boolean to a string

---
+++ 8-Kyu/convert-a-boolean-to-a-string.js
@@ -0,0 +1,12 @@
+//DESCRIPTION:
+//Implement a function which convert the given boolean value into its string representation.
+//Note: Only valid inputs will be given.
+
+function booleanToString(b){
+    if (b == true)
+      return "true"
+    else (b == false)
+    return "false"
+    }
+
+console.log(booleanToString(false))

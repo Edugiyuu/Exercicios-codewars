66e93de5d6e5a64ee0faf9f133dc2dfc12021e78
Edugiyuu <102703153+Edugiyuu@users.noreply.github.com>
2023-04-27 20:23:43 -0300

    Update chuck-norris-IV-bearded-fist.js

--- 7-Kyu/chuck-norris-IV-bearded-fist.js
+++ 7-Kyu/chuck-norris-IV-bearded-fist.js
@@ -5,10 +5,7 @@ When shaving, Chuck accidentally punched himself in the face. He is the only man
 Hidden within the provided sequence of sequences are numbers that represent the letters of the words for some of Chuck's favourite things! Your job is to translate them, and return the words so that Chuck can get back to the business of punching and kicking things.
 */
 function fistBeard(arr) {
-    var arrUnico = arr.flat()
-    console.log(arrUnico);
-    var traducao = String.fromCharCode(...arrUnico)
-    console.log(traducao);
+    return String.fromCharCode(...arr.flat())
   }
 
 console.log(fistBeard([[70, 97, 99], [101, 45, 75, 105, 99, 107]]));
